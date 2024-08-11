bd87b5acdd5ad05c18423b80f9546289e4644218
FXZFun <58504073+FXZFun@users.noreply.github.com>
2024-08-11 13:14:50 -0500

    Add license

--- src/shortsreminder.user.js
+++ src/shortsreminder.user.js
@@ -9,6 +9,7 @@
 // @match        https://*.instagram.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
